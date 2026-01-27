3d323e7abe5fb7771a256591f957723f5691ff17
Filippo-Timo <filtimo2002@gmail.com>
2026-01-27 15:45:55 +0100

    Creates 3 entities

---
+++ src/main/java/filippotimo/entities/Prodotto.java
@@ -0,0 +1,59 @@
+package filippotimo.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "prodotti")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Prodotto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_prodotto")
+    private Long idProdotto;
+
+    @Column(name = "data_emissione", nullable = false)
+    private LocalDate dataEmissione;
+
+    // ********************* COLLEGARE ID RIVENDITORE *********************
+    //    private Rivenditore idRivenditore;
+
+    public Prodotto() {
+    }
+
+    public Prodotto(LocalDate dataEmissione) {
+        this.dataEmissione = dataEmissione;
+        //        this.idRivenditore = idRivenditore;
+    }
+
+    public Long getIdProdotto() {
+        return idProdotto;
+    }
+
+    public LocalDate getDataEmissione() {
+        return dataEmissione;
+    }
+
+    public void setDataEmissione(LocalDate dataEmissione) {
+        this.dataEmissione = dataEmissione;
+    }
+
+//    public Rivenditore getIdRivenditore() {
+//        return idRivenditore;
+//    }
+//
+//    public void setIdRivenditore(long idRivenditore) {
+//        this.idRivenditore = idRivenditore;
+//    }
+
+    @Override
+    public String toString() {
+        return "Prodotto {" +
+                "idProdotto = " + idProdotto +
+                ", dataEmissione = " + dataEmissione +
+//                ", idRivenditore = " + idRivenditore +
+                '}';
+    }
+}
