111a797e24009f484d22a3039b0228e9c9e1e892
Filippo-Timo <filtimo2002@gmail.com>
2026-01-27 12:15:58 +0100

    Corrects the persistence_unit's name

--- src/main/java/filippotimo/Application.java
+++ src/main/java/filippotimo/Application.java
@@ -5,11 +5,11 @@ import jakarta.persistence.Persistence;
 
 public class Application {
 
-    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bw1backendpuu");
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bw1backendpu");
 
 
     public static void main(String[] args) {
-        
+
         System.out.println("Hello World!");
     }
 }
