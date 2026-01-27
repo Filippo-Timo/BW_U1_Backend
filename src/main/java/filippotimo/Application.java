15fb1ca6e23ed8331c37557b8b79e57196c3dd05
Filippo-Timo <filtimo2002@gmail.com>
2026-01-27 12:14:02 +0100

    Adds two dependencies to pom.xml, creates persistence.xml and creates EntityManager into main

--- src/main/java/filippotimo/Application.java
+++ src/main/java/filippotimo/Application.java
@@ -1,8 +1,15 @@
 package filippotimo;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bw1backendpuu");
+
+
     public static void main(String[] args) {
+        
         System.out.println("Hello World!");
     }
 }
