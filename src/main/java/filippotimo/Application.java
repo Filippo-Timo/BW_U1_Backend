c9aa56f31703581d31393b869f7462bf96c6e925
Riccardo <richi-97@live.it>
2026-01-30 16:07:04 +0100

    setup per presentazione

--- src/main/java/filippotimo/Application.java
+++ src/main/java/filippotimo/Application.java
@@ -35,93 +35,99 @@ public class Application {
         UtentiDAO ud = new UtentiDAO(em);
 
 
-        //    *************************************** CREAZIONE RIVENDITORI ***************************************
-
-        Rivenditore primoRivenditore = new RivenditoreAutorizzato("Filippo");
-        Rivenditore secondoRivenditore = new RivenditoreAutomatico("BOT", true);
-
-        // rd.save(primoRivenditore);
-        // rd.save(secondoRivenditore);
-
-
-        //    *************************************** CREAZIONE MEZZO ***************************************
-
-        MezzoPubblico primoMezzo = new MezzoPubblico("Iveco", tipoMezzo.AUTOBUS);
-        MezzoPubblico secondoMezzo = new MezzoPubblico("BOH", tipoMezzo.TRAM);
-
-        // md.saveMezzoPubblico(primoMezzo);
-        // md.saveMezzoPubblico(secondoMezzo);
-
-
-        //    *************************************** CREAZIONE UTENTI E TESSERE ***************************************
-
-        Utenti primoUtente = new Utenti("Marcello", "Lippi");
-        Utenti secondoUtente = new Utenti("Roberto", "Mancini");
-
-        // ud.save(primoUtente);
-        // ud.save(secondoUtente);
-
-
-        // Per le Tessere abbiamo un metodo che esegue la creazione e il salvataggio nel DB in un unico passaggio
-        Utenti primoUtenteFromDB = ud.findUtenteById(1);
-        Utenti secondoUtenteFromDB = ud.findUtenteById(2);
-
-        ud.createAndSaveTessera(primoUtenteFromDB, LocalDate.of(2025, 12, 6));
-        ud.createAndSaveTessera(secondoUtenteFromDB, LocalDate.of(2026, 1, 10));
-
-
-        //    *************************************** CREAZIONE BIGLIETTO ***************************************
-
-        // RIPRENDO I RIVENDITORI DAL DB
-        Rivenditore primoRivenditoreDB = rd.findById(1);
-        Rivenditore secondoRivenditoreDB = rd.findById(2);
-
-        // RIPRENDO I MEZZI DAL DB
-        MezzoPubblico primoMezzoDB = md.findMezzoById(1);
-        MezzoPubblico secondoMezzoDB = md.findMezzoById(2);
-
-        // CREO EFFETTIVAMENTE I BIGLIETTI
-        Biglietto biglietto1 = new Biglietto(LocalDate.of(2026, 1, 12), primoRivenditoreDB, tipoMezzo.AUTOBUS);
-        Biglietto biglietto2 = new Biglietto(LocalDate.of(2026, 1, 29), secondoRivenditoreDB, tipoMezzo.TRAM);
-
-        // pd.save(biglietto1);
-        // pd.save(biglietto2);
-
-
-        //    *************************************** CREAZIONE ABBONAMENTO ***************************************
-
-        Tessera primaTesseraFromDB = ud.findTesseraByNumero(1);
-
-        // Per l'abbonamento abbiamo un metodo che esegue la creazione e il salvataggio nel DB in un unico passaggio
-        pd.createAndSaveAbbonamento(LocalDate.now(), primoRivenditoreDB, durataAbbonamento.MENSILE, primaTesseraFromDB);
-
-
-        //    *************************************** CREAZIONE TRATTA ***************************************
-
-        // Per le tratte abbiamo un metodo che esegue la creazione e il salvataggio nel DB in un unico passaggio
-        td.createAndSaveTratta("Linea A", "Pomezia", "Tor San Lorenzo", 25);
-        td.createAndSaveTratta("Linea B", "Torvaianica", "Tor San Lorenzo", 25);
-
-
-        //    *************************************** CREAZIONE PERCORRENZA ***************************************
-
-        Tratta tratta1DB = td.findTrattaById(1);
-        Tratta tratta2DB = td.findTrattaById(2);
-
-        td.createAndSavePercorrenza(18, tratta1DB, primoMezzoDB);
-        td.createAndSavePercorrenza(18, tratta2DB, secondoMezzoDB);
-
-
-        //    *************************************** CREAZIONE IN MANUTENZIONE ***************************************
-
-        // Per la manutenzione abbiamo un metodo che esegue la creazione e il salvataggio nel DB in un unico passaggio
-        // md.mandaInManutenzione(1);
-        // md.mandaInManutenzione(2);
+//        //    *************************************** CREAZIONE RIVENDITORI ***************************************
+//
+//        Rivenditore primoRivenditore = new RivenditoreAutorizzato("Filippo");
+//        Rivenditore secondoRivenditore = new RivenditoreAutomatico("BOT", true);
+//
+//         rd.save(primoRivenditore);
+//         rd.save(secondoRivenditore);
+//
+//
+//        //    *************************************** CREAZIONE MEZZO ***************************************
+//
+//        MezzoPubblico primoMezzo = new MezzoPubblico("Iveco", tipoMezzo.AUTOBUS);
+//        MezzoPubblico secondoMezzo = new MezzoPubblico("TRAM-1", tipoMezzo.TRAM);
+//        MezzoPubblico terzoMezzo = new MezzoPubblico("Iveco 2", tipoMezzo.AUTOBUS);
+//
+//         md.saveMezzoPubblico(primoMezzo);
+//         md.saveMezzoPubblico(secondoMezzo);
+//         md.saveMezzoPubblico(terzoMezzo);
+//
+//
+//        //    *************************************** CREAZIONE UTENTI E TESSERE ***************************************
+//
+//        Utenti primoUtente = new Utenti("Marcello", "Lippi");
+//        Utenti secondoUtente = new Utenti("Roberto", "Mancini");
+//
+//         ud.save(primoUtente);
+//         ud.save(secondoUtente);
+//
+//
+//        // Per le Tessere abbiamo un metodo che esegue la creazione e il salvataggio nel DB in un unico passaggio
+//        Utenti primoUtenteFromDB = ud.findUtenteById(1);
+//        Utenti secondoUtenteFromDB = ud.findUtenteById(2);
+//
+//        ud.createAndSaveTessera(primoUtenteFromDB, LocalDate.of(2025, 12, 6));
+//        ud.createAndSaveTessera(secondoUtenteFromDB, LocalDate.of(2026, 1, 10));
+//
+//
+//        //    *************************************** CREAZIONE BIGLIETTO ***************************************
+//
+//        // RIPRENDO I RIVENDITORI DAL DB
+//        Rivenditore primoRivenditoreDB = rd.findById(1);
+//        Rivenditore secondoRivenditoreDB = rd.findById(2);
+//
+//        // RIPRENDO I MEZZI DAL DB
+//        MezzoPubblico primoMezzoDB = md.findMezzoById(1);
+//        MezzoPubblico secondoMezzoDB = md.findMezzoById(2);
+//
+//        // CREO EFFETTIVAMENTE I BIGLIETTI
+//        Biglietto biglietto1 = new Biglietto(LocalDate.of(2026, 1, 12), primoRivenditoreDB, tipoMezzo.AUTOBUS);
+//        Biglietto biglietto2 = new Biglietto(LocalDate.of(2026, 1, 29), secondoRivenditoreDB, tipoMezzo.TRAM);
+//        Biglietto biglietto3 = new Biglietto(LocalDate.of(2026, 1, 17), primoRivenditoreDB, tipoMezzo.AUTOBUS);
+//
+//         pd.save(biglietto1);
+//         pd.save(biglietto2);
+//         pd.save(biglietto3);
+//
+//
+//        //    *************************************** CREAZIONE ABBONAMENTO ***************************************
+//
+//        Tessera primaTesseraFromDB = ud.findTesseraByNumero(1);
+//
+//        // Per l'abbonamento abbiamo un metodo che esegue la creazione e il salvataggio nel DB in un unico passaggio
+//        pd.createAndSaveAbbonamento(LocalDate.now(), primoRivenditoreDB, durataAbbonamento.MENSILE, primaTesseraFromDB);
+//
+//
+//        //    *************************************** CREAZIONE TRATTA ***************************************
+//
+//        // Per le tratte abbiamo un metodo che esegue la creazione e il salvataggio nel DB in un unico passaggio
+//        td.createAndSaveTratta("Linea A", "Pomezia", "Tor San Lorenzo", 25);
+//        td.createAndSaveTratta("Linea B", "Torvaianica", "Tor San Lorenzo", 30);
+//
+//
+//        //    *************************************** CREAZIONE PERCORRENZA ***************************************
+//
+//        Tratta tratta1DB = td.findTrattaById(1);
+//        Tratta tratta2DB = td.findTrattaById(2);
+//
+//        td.createAndSavePercorrenza(18, tratta1DB, primoMezzoDB);
+//        td.createAndSavePercorrenza(22, tratta1DB, primoMezzoDB);
+//        td.createAndSavePercorrenza(18, tratta2DB, secondoMezzoDB);
+//        td.createAndSavePercorrenza(33, tratta2DB, secondoMezzoDB);
+//
+//
+//        //    *************************************** CREAZIONE IN MANUTENZIONE ***************************************
+//
+//        // Per la manutenzione abbiamo un metodo che esegue la creazione e il salvataggio nel DB in un unico passaggio
+//         md.mandaInManutenzione(3);
+//        // md.mandaInManutenzione(2);
 
 
         //    *************************************** METODO PER SETTARE LA DATA DI FINE MANUTENZIONE ***************************************
 
-        // md.setDataFineManutenzione(1,LocalDate.of(2025,1,30));
+//         md.setDataFineManutenzione(1,LocalDate.of(2026,1,30));
         // md.setDataFineManutenzione(2,LocalDate.of(2025,1,31));
 
 
@@ -131,7 +137,8 @@ public class Application {
 
         //    *************************************** METODO PER VIDIMARE UN BIGLIETTO ***************************************
 
-        pd.validateATicketBySetter(1, 1);
+//        pd.validateATicketBySetter(1, 1);
+//        pd.validateATicketBySetter(3, 1);
 
 
         /* ----------------------------------------------------------------------------------------------------------------------
@@ -139,12 +146,12 @@ public class Application {
         ---------------------------------------------------------------------------------------------------------------------- */
 
 
-        //    ******************************* VEDERE QUANTI BIGLIETTI E ABBONAMENTI EMESSI PER RIVENDITORE IN UN LASSO DI TEMPO *******************************
+        System.out.println("******************************* VEDERE QUANTI BIGLIETTI E ABBONAMENTI EMESSI PER RIVENDITORE IN UN LASSO DI TEMPO *******************************");
 
-        // Questo metodo restituisce il numero di prodotti
-        pd.countAllProductsInAPeriodOfTime(1, LocalDate.of(2025, 1, 15), LocalDate.now());
+        System.out.println("**Questo metodo restituisce il numero di prodotti**");
+        System.out.println(pd.countAllProductsInAPeriodOfTime(1, LocalDate.of(2025, 1, 15), LocalDate.now()));
 
-        // Questo metodo restituisce una lista di prodotti
+        System.out.println("**Questo metodo restituisce una lista di prodotti**");
         List<Prodotto> listaProdottiEmessi = pd.findAllInAPeriodOfTime(1, LocalDate.of(2025, 1, 15), LocalDate.now());
         if (listaProdottiEmessi.isEmpty()) {
             System.out.println("Nessun prodotto trovato");
@@ -152,41 +159,40 @@ public class Application {
             listaProdottiEmessi.forEach(System.out::println);
         }
 
-        //    ******************************* IMPOSTARE UN RIVENDITORE AUTOMATICO COME IN SERVIZIO / FUORI SERVIZIO *******************************
+//        System.out.println("******************************* IMPOSTARE UN RIVENDITORE AUTOMATICO COME IN SERVIZIO / FUORI SERVIZIO *******************************");
 
         rd.setInServizioRivenditoreAutomatico(2, false);
 
 
-        //    ************************************ VERIFICARE VALIDITÀ ABBONAMENTO IN BASE AL NUMERO DI TESSERA  ************************************
+        System.out.println("************************************ VERIFICARE VALIDITÀ ABBONAMENTO IN BASE AL NUMERO DI TESSERA  ************************************");
 
-        List<Abbonamento> abbonamentoVerificato = ud.verifyAbbonamentoByTessera(2);
+        List<Abbonamento> abbonamentoVerificato = ud.verifyAbbonamentoByTessera(1);
         if (abbonamentoVerificato.isEmpty()) {
             System.out.println("Nessun abbonamento trovato");
         } else {
             abbonamentoVerificato.forEach(System.out::println);
         }
+        System.out.println("************************************ TRACCIARE LE MANUTENZIONI DI UN MEZZO  ************************************");
 
-        //    ************************************ TRACCIARE LE MANUTENZIONI DI UN MEZZO  ************************************
-
-        List<InManutenzione> listaManutenzioniDiUnMezzo = md.getListaManutenzioniPerMezzoById(1);
+        List<InManutenzione> listaManutenzioniDiUnMezzo = md.getListaManutenzioniPerMezzoById(3);
 
         listaManutenzioniDiUnMezzo.forEach(System.out::println);
 
 
-        //    ************************************ NUMERO DI BIGLIETTI VIDIMATI SU UN MEZZO IN UN PERIODO DI TEMPO ************************************
+        System.out.println("************************************ NUMERO DI BIGLIETTI VIDIMATI SU UN MEZZO IN UN PERIODO DI TEMPO ************************************");
 
-        // Ritorna il NUMERO di biglietti vidimati in un determinato lasso di tempo per un determinato mezzo specificato tramite ID
+        System.out.println("--Ritorna il NUMERO di biglietti vidimati in un determinato lasso di tempo per un determinato mezzo specificato tramite ID--");
         long VidimazioniPerMezzoInUnLassoDiTempo = md.getNumeroBigliettiVidimatiPerMezzo(1, LocalDate.of(2025, 1, 5), LocalDate.of(2026, 1, 30));
         System.out.println(VidimazioniPerMezzoInUnLassoDiTempo);
 
 
-        //    *************************************** COUNT / FIND NUMERO DI BIGLIETTI VIDIMATI IN UN DETERMINATO LASSO DI TEMPO ***************************************
+        System.out.println("*************************************** COUNT / FIND NUMERO DI BIGLIETTI VIDIMATI IN UN DETERMINATO LASSO DI TEMPO ***************************************");
 
-        // Ritorna il NUMERO di biglietti vidimati in un determinato lasso di tempo
+        System.out.println("---Ritorna il NUMERO di biglietti vidimati in un determinato lasso di tempo---");
         long numeroBigliettiVidimatiPerTempo = pd.countAllValidatesInAPeriodOfTime(LocalDate.of(2025, 1, 1), LocalDate.of(2026, 12, 31));
         System.out.println(numeroBigliettiVidimatiPerTempo);
 
-        // Ritorna la LISTA di biglietti vidimati in un determinato lasso di tempo
+        System.out.println("---Ritorna la LISTA di biglietti vidimati in un determinato lasso di tempo---");
         List<Biglietto> listaBigliettiVidimatiPerTempo = pd.findAllValidatesInAPeriodOfTime(LocalDate.of(2025, 1, 1), LocalDate.of(2026, 12, 31));
         if (listaBigliettiVidimatiPerTempo.isEmpty()) {
             System.out.println("Nessun biglietto trovato");
@@ -195,14 +201,14 @@ public class Application {
         }
 
 
-        //    ************************************ NUMERO DI PERCORRENZE PER MEZZO E TRATTA ************************************
+        System.out.println("************************************ NUMERO DI PERCORRENZE PER MEZZO E TRATTA ************************************");
 
         long numeroPercorrenzePerMezzoETratta = td.numeroPercorrenzePerMezzoETratta(1, 1);
 
         System.out.println(numeroPercorrenzePerMezzoETratta);
 
 
-        //    ************************************ LISTA DEI TEMPI DI PERCORRENZA EFFETTIVI PER MEZZO E TRATTA ************************************
+        System.out.println("************************************ LISTA DEI TEMPI DI PERCORRENZA EFFETTIVI PER MEZZO E TRATTA ************************************");
 
         List<Percorrenza> listaTempiEffettivi = td.getListaTempiPercorrenzaEffettivi(1, 1);
         if (listaTempiEffettivi.isEmpty()) {
@@ -212,7 +218,7 @@ public class Application {
         }
 
 
-        //    ************************************ TEMPO MEDIO DI PERCORRENZA EFFETTIVO DI UN MEZZO SU UNA TRATTA ************************************
+        System.out.println("************************************ TEMPO MEDIO DI PERCORRENZA EFFETTIVO DI UN MEZZO SU UNA TRATTA ************************************");
 
         double tempoMedioEffettivo = td.tempoMedioPerMezzoETratta(1, 1);
 
